ec10d4e120d8050c09297a852aeaaff8f13660f3
dejanV91 <dejanvukoicic91@gmail.com>
2022-12-10 15:50:19 +0100

    finish display buttons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,49 @@ import React, { useState, useEffect } from "react";
 import { useFetch } from "./useFetch";
 import Follower from "./Follower";
 function App() {
-  return <h2>pagination starter</h2>;
+  const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page]);
+  }, [loading, page]);
+
+  const handlePage = (index) => {
+    setPage(index);
+  };
+
+  return (
+    <main>
+      <div className="section-title">
+        <h1>{loading ? "Loading..." : "Pagination"}</h1>
+        <div className="underline"></div>
+      </div>
+      <div className="followers">
+        <div className="container">
+          {followers.map((follower) => {
+            return <Follower key={follower.id} {...follower} />;
+          })}
+        </div>
+      </div>
+      {!loading && (
+        <div className="btn-container">
+          {data.map((item, index) => {
+            return (
+              <button
+                key={index}
+                className={`page-btn ${page === index ? "active-btn" : "null"}`}
+                onClick={() => handlePage(index)}
+              >
+                {index + 1}
+              </button>
+            );
+          })}
+        </div>
+      )}
+    </main>
+  );
 }
 
 export default App;
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,12 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const numberOnPage = 10;
+  const itemsOnPages = Math.ceil(followers.length / numberOnPage);
 
-export default paginate
+  const newArray = Array.from({ length: itemsOnPages }, (_, index) => {
+    let start = numberOnPage * index;
+    return followers.slice(start, start + numberOnPage);
+  });
+  return newArray;
+};
+
+export default paginate;
